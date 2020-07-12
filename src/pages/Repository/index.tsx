b4af960e6ab8c02e0d1c021153ec40cd52caf1ef
Ruan Valente <ruanopensuselinux@gmail.com>
2020-07-11 23:05:13 -0300

    initial and mock structure page detailthe repository :memo

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import {useRouteMatch} from 'react-router-dom'
+
+import LogoImage from '../../assets/logo.svg'
+import {FiChevronLeft} from 'react-icons/fi'
+
+import {Container} from './styles'
+
+interface RepositoryParams {
+  repository: string
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>
+  const {params} = useRouteMatch<RepositoryParams>()
+  return (
+    <>
+      <Container>
+        <img src={LogoImage} alt="GitHub Explore"/>
+
+        <div>
+          <FiChevronLeft size={20}/>
+          <p>Voltar</p>
+        </div>
+      </Container>
+
+      <div>
+
+      <img src="https://avatars1.githubusercontent.com/u/6674232?s=400&u=344a7d5b79c79db1a77c0830bcf68bf68e2d9694&v=4" alt="Profile image"/>
+        <h1>{params.repository}</h1>
+        <div>
+          <p>Desription the repository</p>
+        </div>
+      </div>
+
+    </>
+  )
 }
 
 export default Repository
---
+++ src/pages/Repository/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 40px;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    p {
+      color: #a8a8b3;
+    }
+  }
+  svg {
+    color: #a8a8b3;
+  }
+`
