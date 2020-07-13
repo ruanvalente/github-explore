7e239776f66a4bdb0d6f04066ef6986612afb9f5
Ruan Valente <ruanopensuselinux@gmail.com>
2020-07-12 21:11:11 -0300

    list page structure of the finished repository issues and added page styles :memo

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {useRouteMatch} from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom'
 
 import LogoImage from '../../assets/logo.svg'
-import {FiChevronLeft} from 'react-icons/fi'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
-import {Container} from './styles'
+import { Header, RepositoryInfo, Issues } from './styles'
 
 interface RepositoryParams {
   repository: string
@@ -14,24 +14,62 @@ const Repository: React.FC = () => {
   const {params} = useRouteMatch<RepositoryParams>()
   return (
     <>
-      <Container>
+      <Header>
         <img src={LogoImage} alt="GitHub Explore"/>
+          <Link to="/">
+            <FiChevronLeft size={20}/>
+            <p>Voltar</p>
+          </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars3.githubusercontent.com/u/6674232?v=4" alt="Profile Info"/>
+          <div>
+            <strong>user/repository</strong>
+            <p>Repository description</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1809</strong>
+            <span>Star</span>
+          </li>
 
-        <div>
-          <FiChevronLeft size={20}/>
-          <p>Voltar</p>
-        </div>
-      </Container>
+          <li>
+            <strong>90</strong>
+            <span>Forks</span>
+          </li>
 
-      <div>
+          <li>
+            <strong>45</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
 
-      <img src="https://avatars1.githubusercontent.com/u/6674232?s=400&u=344a7d5b79c79db1a77c0830bcf68bf68e2d9694&v=4" alt="Profile image"/>
-        <h1>{params.repository}</h1>
-        <div>
-          <p>Desription the repository</p>
-        </div>
-      </div>
+      <Issues>
+        <Link
+          to={'/'}
+        >
+          <div>
+            <strong>Title issue</strong>
+              <p>Description issue</p>
+            </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
 
+      <Issues>
+        <Link
+          to={'/'}
+        >
+          <div>
+            <strong>Title issue</strong>
+              <p>Description issue</p>
+            </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </>
   )
 }
--- src/pages/Repository/styles.ts
+++ src/pages/Repository/styles.ts
@@ -1,22 +1,128 @@
+import { shade } from 'polished';
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Header = styled.header`
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  margin-bottom: 40px;
+  justify-content: space-between;
 
-  div {
+  a {
     display: flex;
-    justify-content: center;
     align-items: center;
-    cursor: pointer;
+    justify-content: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.2s;
 
-    p {
-      color: #a8a8b3;
+    &:hover {
+      color: ${shade(0.2, '#a8a8b3')}
+    }
+
+    svg {
+      margin-right: 12px;
     }
   }
-  svg {
-    color: #a8a8b3;
+`
+
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 30px;
+
+      strong {
+        font-size: 36px;
+        line-height: 42px;
+        color: #3D3D4D;
+      }
+
+      p {
+        font-size: 18px;
+        line-height: 23px;
+        color: #737380;
+        margin-top: 8px;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    margin: 40px;
+    list-style: none;
+
+    li {
+
+      & + li {
+        margin-left: 80px;
+      }
+
+      strong {
+        display: block;
+        font-size: 36px;
+        color: #3D3D4D;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #6c6c80;
+      }
+    }
+  }
+`
+
+export const Issues = styled.div`
+  margin-top: 16px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    div {
+      flex: 1;
+      margin: 0 16px;
+
+      strong {
+        font-size: 16px;
+        color: #3d3d4d;
+        line-height: 28px;
+      }
+
+      p {
+        font-size: 14px;
+        color: #a8a8b3;
+        line-height: 21px;
+      }
+    }
+    svg {
+      margin-left: auto;
+      color: #a8a8b3;
+    }
   }
 `
